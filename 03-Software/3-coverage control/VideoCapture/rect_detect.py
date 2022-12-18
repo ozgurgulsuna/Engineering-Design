217bf4ecbde00641f18dcae2da2ede74852a77a2
feyzaanurr <72084658+feyzaanurr@users.noreply.github.com>
2022-12-18 17:05:01 +0300

    Video capture and image comparison files are merged
    Video capture and image comparison files are merged. hmmmm.py is converted to a function in a new file img_compare.py.

---
+++ 03-Software/3-coverage control/VideoCapture/rect_detect.py
@@ -0,0 +1,32 @@
+import cv2 as cv
+import numpy as np
+import time
+import largestinteriorrectangle as lir
+
+def rect_detect():
+    start = time.time()
+    # Import your picture
+    img = cv.imread("Untitled2.png",cv.IMREAD_COLOR)
+    # Color it in gray
+    mask = 255-cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    # Create our mask by selecting the non-zero values of the picture
+    _, tresh = cv.threshold(mask,0,255,cv.THRESH_BINARY)
+    # Select the contour
+    contours,_ = cv.findContours(tresh, cv.RETR_CCOMP, cv.CHAIN_APPROX_NONE)
+    # if your mask is incurved or if you want better results, 
+    # you may want to use cv2.CHAIN_APPROX_NONE instead of cv2.CHAIN_APPROX_SIMPLE, 
+    # but the rectangle search will be longer
+    cv.drawContours(img, contours,0, (0, 0, 255), 1)
+    contour = contours[0][:, 0, :]
+    grid=mask>0
+    rect=lir.lir(grid,contour)
+    blank=np.zeros(mask.shape,dtype="uint8")
+    print(contour.shape)
+    print(rect)
+    cv.imshow('rect',cv.rectangle(blank,[rect[0],rect[1]],[rect[2]+rect[0],rect[3]+rect[1]],255,1))
+
+    cv.imshow("mask", mask)
+    cv.imshow("img", img)
+    end = time.time()
+    print(end - start)
+    cv.waitKey(0)
