bd74a0217cb662ad16c46ae9f98323f0eb3a1df3
Özgür Gülsuna <ozgurgulsuna@gmail.com>
2023-05-07 19:40:55 +0300

    camera test w/out opencv

--- 03-Software/3-CoverageControl/TEST/cam_test.py
+++ 03-Software/3-CoverageControl/TEST/cam_test.py
@@ -1,25 +1,17 @@
-# import the necessary packages
-from picamera.array import PiRGBArray
+from time import sleep
 from picamera import PiCamera
-import time
-import cv2
-# initialize the camera and grab a reference to the raw camera capture
-camera = PiCamera()
-camera.resolution = (640, 480)
-camera.framerate = 32
-rawCapture = PiRGBArray(camera, size=(640, 480))
-# allow the camera to warmup
-time.sleep(0.1)
-# capture frames from the camera
-for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-	# grab the raw NumPy array representing the image, then initialize the timestamp
-	# and occupied/unoccupied text
-	image = frame.array
-	# show the frame
-	cv2.imshow("Frame", image)
-	key = cv2.waitKey(1) & 0xFF
-	# clear the stream in preparation for the next frame
-	rawCapture.truncate(0)
-	# if the `q` key was pressed, break from the loop
-	if key == ord("q"):
-		break
+
+camera = PiCamera(resolution=(1280, 720), framerate=30)
+camera.led = False
+# Set ISO to the desired value
+camera.iso = 100
+# Wait for the automatic gain control to settle
+sleep(2)
+# Now fix the values
+camera.shutter_speed = camera.exposure_speed
+camera.exposure_mode = 'off'
+g = camera.awb_gains
+camera.awb_mode = 'off'
+camera.awb_gains = g
+# Finally, take several photos with the fixed settings
+camera.capture_sequence(['image%02d.jpg' % i for i in range(10)])
